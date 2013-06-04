bf6c091c3b10e386bfdd3d7f0b13514362fd2441
Alex Osborne <aosborne@nla.gov.au>
2013-06-04 16:22:05 +1000

    wayback.urlprefix requires a trailing slash

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -6,7 +6,7 @@ fi
 
 sed -i "s@^agwa.host: .*@agwa.host: $AGWA_HOST@" WEB-INF/agwa.properties
 sed -i "s@^agwa.port: .*@agwa.port: $AGWA_PORT@" WEB-INF/agwa.properties
-sed -i "s@wayback.urlprefix=.*@wayback.urlprefix=http://$(hostname -f):$PORT/wayback@" WEB-INF/wayback.xml
+sed -i "s@wayback.urlprefix=.*@wayback.urlprefix=http://$(hostname -f):$PORT/wayback/@" WEB-INF/wayback.xml
 sed -i "s@8080@$PORT@g" WEB-INF/wayback.xml
 sed -i "s@/data/gov2011-12/path-index.txt@$AGWA_PATH_INDEX@g" WEB-INF/wayback.xml WEB-INF/CDXCollection.xml
 mkdir $1/ROOT
