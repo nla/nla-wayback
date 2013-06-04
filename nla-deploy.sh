2a897e194fe367a7888b89b6d46e09d6fa1fc771
Alex Osborne <aosborne@nla.gov.au>
2013-06-04 15:56:05 +1000

    Add deploy script

---
+++ nla-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -z "$AGWA_HOST" ]; then
+  echo "The AGWA_HOST and AGWA_PORT environments need to be set in your jettyctl config file."
+  exit 1
+fi
+
+sed -i "s@^agwa.host: .*@agwa.host: $AGWA_HOST@" WEB-INF/agwa.properties
+sed -i "s@^agwa.port: .*@agwa.port: $AGWA_PORT@" WEB-INF/agwa.properties
+mkdir $1/ROOT
+cp -a ./ $1/ROOT/
