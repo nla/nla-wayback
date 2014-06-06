7f54e71fa32c3e503dcd158a7b0d7b3be28ff2a8
Alex Osborne <aosborne@nla.gov.au>
2014-06-06 15:15:20 +1000

    Upgrade to OpenWayback 2.0 beta 2
    This reworks the project to be OpenWayback Maven overlay.
    We only keep files that have been modified.  This makes upgrades
    easier: just change the version in pom.xml.  It's also the
    customisation method recommended by the OpenWayback project.
    
    Removed all checked in binaries.
    
    Configuration has changed upstream.  Rebased on the upstream
    Spring XML as recommended in the OpenWayback upgrade guide. Some
    customisations may need to be reapplied.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 govau-wayback
 =============
 
-Lightly modified Wayback machine for use with Australian Government 
+Lightly modified [OpenWayback](https://github.com/iipc/openwayback) overlay for use with Australian Government
 Web Archive (AGWA) web application. 
 
-The files in this project are mainly from the Open Source Wayback
-Machine version 1.7.1 and come from WAR file *wayback-1.7.1-SNAPSHOT.war*
-contained in the root folder of the distribution file
-*dist-1.7.1-SNAPSHOT-1.7.1-SNAPSHOT.tar.gz*.
+Changes
+-------
 
-The initial checkin consists of the virgin files from the WAR file
-mentioned above. The customisations for use with AGWA are checked in
-as a revision so that it is easier to see what has been modified.
+* Injected JavaScript which uses PostMessage to pass page details to AGWA
+* Removed toolbar (AGWA has it's own in outer frame')
+* NLA branded ArchiveComment.jsp
+* Simplified styling on error pages
+
--- nla-deploy.sh
+++ nla-deploy.sh
@@ -6,11 +6,11 @@ fi
 
 # WAYBACK_URL *must* be specified as a relative URI, e.g. '/gov/wayback/'
 
-sed -i "s@^agwa.url: .*@agwa.url: $AGWA_URL@" WEB-INF/agwa.properties
-sed -i "s@^wayback.url: .*@wayback.url: ${WAYBACK_URL%/}@" WEB-INF/agwa.properties
-sed -i "s@wayback.urlprefix=.*@wayback.urlprefix=${WAYBACK_URL%/}/@" WEB-INF/wayback.xml
+sed -i "s@^agwa.url: .*@agwa.url: $AGWA_URL@" src/main/webapp/WEB-INF/agwa.properties
+sed -i "s@^wayback.url: .*@wayback.url: ${WAYBACK_URL%/}@" src/main/webapp/WEB-INF/agwa.properties
+sed -i "s@wayback.urlprefix=.*@wayback.urlprefix=${WAYBACK_URL%/}/@" src/main/webapp/WEB-INF/wayback.xml
 
-sed -i "s@8080@$PORT@g" WEB-INF/wayback.xml
-sed -i "s@/data/agwa/indexes/agwa-path-index.txt@$AGWA_PATH_INDEX@g" WEB-INF/wayback.xml WEB-INF/CDXCollection.xml
+sed -i "s@8080@$PORT@g" src/main/webapp/WEB-INF/wayback.xml
+sed -i "s@/data/agwa/indexes/agwa-path-index.txt@$AGWA_PATH_INDEX@g" src/main/webapp/WEB-INF/wayback.xml src/main/webapp/WEB-INF/CDXCollection.xml
 mkdir $1/ROOT
 cp -a ./ $1/ROOT/
