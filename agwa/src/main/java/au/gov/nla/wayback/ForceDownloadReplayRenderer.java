e107787d9f38f74eaf5064d6225174c0c4371b47
Alex Osborne <aosborne@nla.gov.au>
2014-09-04 17:18:34 +1000

    Force PDF files to be downloaded
    Workaround for the sandbox blocking PDF rendering in Chrome and Safari

---
+++ agwa/src/main/java/au/gov/nla/wayback/ForceDownloadReplayRenderer.java
@@ -0,0 +1,36 @@
+package au.gov.nla.wayback;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.archive.wayback.ResultURIConverter;
+import org.archive.wayback.core.CaptureSearchResult;
+import org.archive.wayback.core.CaptureSearchResults;
+import org.archive.wayback.core.Resource;
+import org.archive.wayback.core.WaybackRequest;
+import org.archive.wayback.exception.BadContentException;
+import org.archive.wayback.replay.HttpHeaderProcessor;
+import org.archive.wayback.replay.TransparentReplayRenderer; 
+
+public class ForceDownloadReplayRenderer extends TransparentReplayRenderer {
+	
+    public ForceDownloadReplayRenderer(HttpHeaderProcessor httpHeaderProcessor) {
+        super(httpHeaderProcessor);
+    }
+
+	@Override
+	public void renderResource(HttpServletRequest httpRequest,
+			HttpServletResponse httpResponse, WaybackRequest wbRequest,
+			CaptureSearchResult result, Resource httpHeadersResource,
+			Resource payloadResource, ResultURIConverter uriConverter,
+			CaptureSearchResults results) throws ServletException, IOException,
+			BadContentException {
+		httpResponse.addHeader("Content-Disposition", "attachment");
+		super.renderResource(httpRequest, httpResponse, wbRequest, result,
+				httpHeadersResource, payloadResource, uriConverter, results);
+	}
+
+}
