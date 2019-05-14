41da59af0e6c48d0ea33203c13f2fa15f19b6226
Alex Osborne <aosborne@nla.gov.au>
2019-05-14 15:51:34 +0900

    Upgrade OpenWayback to 2.4.0

--- trove/src/main/java/au/gov/nla/wayback/ForceDownloadReplayRenderer.java
+++ trove/src/main/java/au/gov/nla/wayback/ForceDownloadReplayRenderer.java
@@ -11,7 +11,7 @@ import org.archive.wayback.core.CaptureSearchResult;
 import org.archive.wayback.core.CaptureSearchResults;
 import org.archive.wayback.core.Resource;
 import org.archive.wayback.core.WaybackRequest;
-import org.archive.wayback.exception.BadContentException;
+import org.archive.wayback.exception.SpecificCaptureReplayException;
 import org.archive.wayback.replay.HttpHeaderProcessor;
 import org.archive.wayback.replay.TransparentReplayRenderer; 
 
@@ -27,7 +27,7 @@ public class ForceDownloadReplayRenderer extends TransparentReplayRenderer {
 			CaptureSearchResult result, Resource httpHeadersResource,
 			Resource payloadResource, ResultURIConverter uriConverter,
 			CaptureSearchResults results) throws ServletException, IOException,
-			BadContentException {
+			SpecificCaptureReplayException {
 		httpResponse.addHeader("Content-Disposition", "attachment");
 		super.renderResource(httpRequest, httpResponse, wbRequest, result,
 				httpHeadersResource, payloadResource, uriConverter, results);
